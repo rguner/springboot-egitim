6435b7ad6153c4803559818d7a3103392c7aeb5d
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2020-06-17 17:27:28 +0300

    zemberek dependencies

---
+++ zemberek/src/test/java/com/example/zemberek/ZemberekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zemberek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZemberekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
