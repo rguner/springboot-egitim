40e1f8529c3bd0fde406e8f9a3311fd416a72787
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2020-07-09 13:45:49 +0300

    security

---
+++ websecurity/src/main/java/com/example/websecurity/WebsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.websecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsecurityApplication.class, args);
+	}
+
+}
