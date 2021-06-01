b844fecdae0e1634d02f90ed5bfacfd2e3c36b59
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2021-06-01 14:21:00 +0300

    bilginç eğitim

---
+++ springbilginc1/src/main/java/com/example/springbilginc1/Springbilginc1Application.java
@@ -0,0 +1,13 @@
+package com.example.springbilginc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbilginc1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbilginc1Application.class, args);
+    }
+
+}
---
+++ springbilginc1/src/test/java/com/example/springbilginc1/Springbilginc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbilginc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbilginc1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
